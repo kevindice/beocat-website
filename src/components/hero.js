c5e9294ecb7bf97ce8747ec40e570908bee84cfe
Kevin Dice <kevin.dice1@gmail.com>
2019-01-24 13:30:24 -0600

    Implement a minimal React version of the hero slider

---
+++ src/components/hero.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Swiper from 'react-id-swiper'
+import 'react-id-swiper/src/styles/scss/swiper.scss'
+
+const Hero = () => (
+  <section className="hero hero-home">
+
+    <Swiper
+      navigation={{ nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' }}
+      pagination={{ el: '.swiper-pagination', clickable: true }}
+    >
+
+      <div>
+        <p>Testing 123</p>
+      </div>
+
+      <div>
+        <p>Testing 456</p>
+      </div>
+
+    </Swiper>
+
+  </section>
+)
+
+export default Hero
