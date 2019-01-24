fed96ff7c80867f747b25eb920d250824ac09a1a
Kevin Dice <kevin.dice1@gmail.com>
2019-01-24 12:20:33 -0600

    Update title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Beocat`,
+    description: `Research Computing at Kansas State University`,
+    author: `Institute for Computational Research in Engineering and SCience`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
